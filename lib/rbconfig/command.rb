f40299de945804aada1437d2b9c453d9405202d5
tarao <tarao.gnn@gmail.com>
2009-10-19 13:04:38 +0000

    Exerb support.

--- lib/rbconfig/command.rb
+++ lib/rbconfig/command.rb
@@ -4,15 +4,19 @@ module RbConfig
   cmd = File.join(CONFIG['bindir'], CONFIG['ruby_install_name'])
   CONFIG['ruby_command'] = cmd.sub(/.*\s.*/m, '"\&"')
 
+  def program_name
+    return (defined?(ExerbRuntime) && ExerbRuntime.filepath) || $0
+  end
+
   def self_invoke_command
-    cmd = File.expand_path($0)
+    cmd = File.expand_path(program_name)
     cmd.sub(/.*\s.*/m, '"\&"')
     ext = Config::CONFIG['EXEEXT']
-    unless ext.length != 0 && $0 =~ /#{ext}$/
+    unless ext.length != 0 && program_name =~ /#{ext}$/
       cmd = CONFIG['ruby_command'] + ' ' + cmd
     end
     return cmd
   end
 
-  module_function :self_invoke_command
+  module_function :program_name, :self_invoke_command
 end
